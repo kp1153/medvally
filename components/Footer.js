55ff25e672f0b5f7f4c7b81846c48a15d2e732ef
kp1153 <kamta_tiwari@yahoo.com>
2025-10-25 13:52:43 +0530

    force deploy

--- components/Footer.js
+++ components/Footer.js
@@ -8,22 +8,15 @@ export default function Footer() {
           © {new Date().getFullYear()} Medvalley. All Rights Reserved.
         </p>
         <p className="text-sm mb-4">Trusted Medical Tourism Company in India</p>
-
-        <p className="text-xs text-indigo-200 mt-4">
-          Contact:{" "}
-          <a href="mailto:medvalleypvtltd@gmail.com" className="underline">
-            medvalleypvtltd@gmail.com
-          </a>{" "}
-          • +91-9523534038
-        </p>
       </div>
 
       <div className="mt-4 text-left px-4">
-        <a
+      <a
+        
           href="https://www.web-developer-kp.com"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-xs text-pink-400 font-semibold hover:text-white transition underline"
+          className="text-xs text-indigo-300 hover:text-white transition"
           aria-label="Web Developer Kamta Prasad - Visit website"
         >
           Web Developer: Kamta Prasad
